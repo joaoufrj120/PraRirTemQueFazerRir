87ddb6e58facee2937e39990555e031aee1f4a37
Slaziel <joao.almeida@poli.ufrj.br>
2024-01-27 11:49:24 -0300

    PlayerAnimation

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -8,16 +8,31 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D player;
     public float speed;
 
+    Animator anim;
+    private Vector2 lastMoveDirection, stopped;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        stopped = new Vector2(0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         player.velocity = movement.normalized * speed;
+        if(movement != stopped)
+        {
+            lastMoveDirection = movement;
+        }
+        anim.SetFloat("xSpeed", movement.x);
+        anim.SetFloat("ySpeed", movement.y);
+        anim.SetFloat("MoveMagnitude", movement.magnitude);
+        anim.SetFloat("LastMoveX", lastMoveDirection.x);
+        anim.SetFloat("LastMoveY", lastMoveDirection.y);
     }
+    
 }
